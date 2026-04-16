41670951d02a7633c37eeded2ab7fd4fab24c165
jatshivraj371-ship-it <jatshivraj371@gmail.com>
2026-04-17 01:57:44 +0530

    Add files via upload

---
+++ first program.py
@@ -0,0 +1,20 @@
+name= "shiv"
+age= 19
+price= 35.00
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+
+
+
+
+
+
+
+a=float(input ("enter first"))
+b=float(input("enter second"))
+
+print=("avg=",(a+b)/2)
+
